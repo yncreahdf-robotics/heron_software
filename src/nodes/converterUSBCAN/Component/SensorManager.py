339f44dd96863736134225f577621558b00c59ce
Gillou100 <gilles.hubert@isen.yncrea.fr>
2019-11-15 14:33:20 +0100

    Create SensorManager class.

---
+++ src/nodes/converterUSBCAN/Component/SensorManager.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+class SensorManager:
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        pass
+    except KeyboardInterrupt:
+        pass
+    finally:
+        pass
