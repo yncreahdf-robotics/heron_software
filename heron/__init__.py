a4a4ba120de6295d7a1ae9d5d9c02513ffb354bc
Gillou100 <gilles.hubert@isen.yncrea.fr>
2019-11-18 18:41:27 +0100

    Remove hello test in python librairy.

--- heron/__init__.py
+++ heron/__init__.py
@@ -6,6 +6,10 @@ try:
     from serial import Serial
 except ImportError:
     from os import system
+    print("Please, accept to install:\n" + \
+        "\t python3-pip and pyton librairies \"pyserial\" and \"rospkg\"\n\n" + \
+        "in order to use ower ROS package.")
+    system("sudo apt install python3-pip")
     system("python3 -m pip install pyserial rospkg --user")
 
 from heron.hello import coucou
