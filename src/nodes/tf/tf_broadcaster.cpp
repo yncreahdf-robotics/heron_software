e905917a9cfcdcfa6fecd64c41ad964301113202
charles <charles.goudaert@isen.yncrea.fr>
2019-11-21 10:47:40 +0100

    tf imu

--- src/nodes/tf/tf_broadcaster.cpp
+++ src/nodes/tf/tf_broadcaster.cpp
@@ -7,13 +7,21 @@ int main(int argc, char** argv){
 
   ros::Rate r(100);
 
-  tf::TransformBroadcaster broadcaster;
+  tf::TransformBroadcaster laser_broadcaster;
+  tf::TransformBroadcaster imu_broadcaster;
 
   while(n.ok()){
-    broadcaster.sendTransform(
+    laser_broadcaster.sendTransform(
       tf::StampedTransform(
         tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.2)),
         ros::Time::now(),"base_link", "laser"));
+
+    imu_broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.09)),
+        ros::Time::now(),"base_link", "imu"));
+
+
     r.sleep();
   }
 }
