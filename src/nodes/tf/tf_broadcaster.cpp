6d837abe36574529bd2482b6fd48ed2cbacb0c7c
Sylvain Louvet <sylvain.louvet@isen.yncrea.fr>
2019-12-02 13:51:21 +0100

    TF

--- src/nodes/tf/tf_broadcaster.cpp
+++ src/nodes/tf/tf_broadcaster.cpp
@@ -7,10 +7,22 @@ int main(int argc, char** argv){
 
   ros::Rate r(100);
 
+  tf::TransformBroadcaster base_broadcaster;
+  tf::TransformBroadcaster plate_broadcaster;
   tf::TransformBroadcaster laser_broadcaster;
   tf::TransformBroadcaster imu_broadcaster;
 
   while(n.ok()){
+    base_broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0, 0, 0.1)),
+        ros::Time::now(),"odom", "base_link"));
+
+    plate_broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0, 0, 0)),
+        ros::Time::now(),"base_link", "plate"));
+
     laser_broadcaster.sendTransform(
       tf::StampedTransform(
         tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.2)),
