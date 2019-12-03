7ac9fd2d7e713877368ca9bbaabd0de7bbacb59d
charles <charles.goudaert@isen.yncrea.fr>
2019-12-03 17:29:54 +0100

    transforms sensors

--- src/nodes/tf/tf_broadcaster.cpp
+++ src/nodes/tf/tf_broadcaster.cpp
@@ -10,15 +10,21 @@ int main(int argc, char** argv){
   tf::TransformBroadcaster laser_broadcaster;
   tf::TransformBroadcaster imu_broadcaster;
 
+  tf::Quaternion laser_quat;
+  laser_quat.setRPY(0, 0, M_PI);
+
+  tf::Quaternion imu_quat;
+  imu_quat.setRPY(0, 0, M_PI);
+
   while(n.ok()){
     laser_broadcaster.sendTransform(
       tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.2)),
+        tf::Transform(laser_quat, tf::Vector3(0.0, 0.0, 0.2)),
         ros::Time::now(),"base_link", "laser"));
 
     imu_broadcaster.sendTransform(
       tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.09)),
+        tf::Transform(imu_quat, tf::Vector3(0.0, 0.0, 0.09)),
         ros::Time::now(),"base_link", "imu"));
 
 
