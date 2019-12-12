04526fa8a7f2e1cbee993a29698dd2088a68540f
Sylvain Louvet <sylvain.louvet@isen.yncrea.fr>
2019-12-12 17:25:41 +0100

    Change tf_robot for using joint_state_publisher

--- src/nodes/tf/tf_robot.cpp
+++ src/nodes/tf/tf_robot.cpp
@@ -3,68 +3,52 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include "heron/winch.h"
-#include <string.h>
+#include <sensor_msgs/JointState.h>
+#define MAX_PLATE 1071.0
+#define MIN_PLATE 695.0
 
 using namespace std;
-float plate_height;
+sensor_msgs::JointState joint_state;
+ros::Publisher plate_pub;
+
 
 void winchCallback(const heron::winch& msg) 
   {
-    plate_height = (msg.height - 695) / 1000;
-    cout << plate_height << std::endl;
+    float plate_height;
+    if (msg.height > MAX_PLATE)
+    {
+      plate_height = (MAX_PLATE - MIN_PLATE) / 1000;
+    }
+    else if (msg.height < MIN_PLATE)
+    {
+      plate_height = 0;
+    }
+    else
+    {
+      plate_height = (msg.height - MIN_PLATE) / 1000;
+    }
+
+    plate_pub.publish(joint_state);
+    joint_state.position[0]=plate_height;
   };
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "robot_tf_publisher_pieces");
   ros::NodeHandle n;
-  ros::Subscriber sub; 
+  ros::Subscriber sub;
 
   tf::TransformBroadcaster base_broadcaster;
-  tf::TransformBroadcaster plate_broadcaster;
-  tf::TransformBroadcaster support_broadcaster;
-  tf::TransformBroadcaster wheel_FR_broadcaster;
-  tf::TransformBroadcaster wheel_FL_broadcaster;
-  tf::TransformBroadcaster wheel_BR_broadcaster;
-  tf::TransformBroadcaster wheel_BL_broadcaster;
-
+  joint_state.name.push_back("plate");
+  joint_state.position.push_back(0);
   while(n.ok()){
     ros::spinOnce();
-
-    sub = n.subscribe("winch_Height", 10, winchCallback);
-    
     base_broadcaster.sendTransform(
       tf::StampedTransform(
         tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0, 0, 0.1)),
         ros::Time::now(),"odom", "base_link"));
 
-    plate_broadcaster.sendTransform(
-      tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.32, 0, 0.13 + plate_height)),
-        ros::Time::now(),"base_link", "plate"));
-    
-    support_broadcaster.sendTransform(
-      tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.3, 0, 0)),
-        ros::Time::now(),"base_link", "support"));
-    
-    wheel_FR_broadcaster.sendTransform(
-      tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.2, -0.216235, -0.06)),
-        ros::Time::now(),"base_link", "wheel_FR"));
-    
-    wheel_FL_broadcaster.sendTransform(
-      tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.2, 0.216235, -0.06)),
-        ros::Time::now(),"base_link", "wheel_FL"));
-    
-    wheel_BR_broadcaster.sendTransform(
-      tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.2, -0.216235, -0.06)),
-        ros::Time::now(),"base_link", "wheel_BR"));
-    
-    wheel_BL_broadcaster.sendTransform(
-      tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.2, 0.216235, -0.06)),
-        ros::Time::now(),"base_link", "wheel_BL"));
+    sub = n.subscribe("winch_Height", 10, winchCallback);
+    plate_pub = n.advertise<sensor_msgs::JointState>("plate_joint_states",10);
+
   }
 }
