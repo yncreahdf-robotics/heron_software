34b4cbe404622fc93e11f8c8fc4f97cc506f6df9
charles <charles.goudaert@isen.yncrea.fr>
2019-12-16 10:12:05 +0100

    remote to start the controller

--- src/nodes/remote/remote.py
+++ src/nodes/remote/remote.py
@@ -5,40 +5,43 @@ import os, sys
 import roslaunch
 
 
-switch = False
+controller_switch = False
 flag = False
 
-uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-roslaunch.configure_logging(uuid)
-launch = roslaunch.parent.ROSLaunchParent(uuid,[str(os.getcwd())+"/catkin_ws/src/heron_software/src/launch/xboxController.launch"])
-
 def callback(data):
-    global switch
+    global controller_switch
     global flag
+    # left menu button
     if(data.buttons[6]):
-        switch = not(switch)
+        controller_switch = not(controller_switch)
         flag = True
 
 
 if __name__ == '__main__':
     # starts the node
-    rospy.init_node('remote')
+    rospy.init_node('remote', log_level = rospy.INFO)
 
     # subscribed to joystick inputs on topic "joy"
     rospy.Subscriber("joy", Joy, callback)
-    progstop = True
-    while not rospy.is_shutdown() and progstop:
+
+    while not rospy.is_shutdown():
 
         if(flag):
-            if(switch):
-                launch.start()
-                print("launching controller")
+
+            # start xboxController to pilot the robot
+            if(controller_switch):
+                uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+                roslaunch.configure_logging(uuid)
+                controller_launch = roslaunch.parent.ROSLaunchParent(uuid,[str(os.getcwd())+"/catkin_ws/src/heron_software/src/launch/xboxController.launch"])
+
+                controller_launch.start()
+                rospy.loginfo("launching controller")
 
             else:
-                launch.shutdown()
-                print("shutting down controller")
-                progstop = False
-        
+                controller_launch.shutdown()
+                rospy.loginfo("shutting down controller")        
+
             flag = False
+
         rospy.sleep(1)
     
