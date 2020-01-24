8508ad0d145b64426136c9682325e54dfe859f7f
HeronISEN <heronisen@gmail.com>
2020-01-24 16:43:58 +0100

    record pos into txt file

--- src/nodes/remote/recordPositions.py
+++ src/nodes/remote/recordPositions.py
@@ -5,29 +5,33 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 
 positionMsg = str()
 index = 0
+tmp_pos = str()
 
 def callback(data):
     global index
+    global tmp_pos 
     # left menu button
-    if(data.buttons[6]):
-        file = open("../../../map/positions.txt", "a")
-        file.write("position " + str(index) + " ")
-        file.write(positionMsg)
-        file.close()
-        index += 1
+    if(data.buttons[0]):
+        if(tmp_pos != positionMsg):
+            file = open("../MapKeyPos.txt", "a")
+            file.write("position " + str(index) + " ")
+            file.write(positionMsg)
+            print("writting Position" + str(index) + "into the file.")
+            file.close()
+            index += 1
+        tmp_pos = positionMsg
 
 def getPos(data):
     global positionMsg
-    positionMsg = str(data.pose.pose.position.x) + ";" + str(data.pose.pose.position.y) + ";" + str(data.pose.pose.orientation.z) + ";" + str(data.pose.pose.orientation.w) + "/n"
+    positionMsg = str(data.pose.pose.position.x) + ";" + str(data.pose.pose.position.y) + ";" + str(data.pose.pose.orientation.z) + ";" + str(data.pose.pose.orientation.w) + "\n"
 
 
 if __name__ == '__main__':
+    file = open("../MapKeyPos.txt", "w")
+    file.write("Next follows the positions on the map registered in the same order than recorded \n")
+    file.close()
     # starts the node
     rospy.init_node('remote')
-
-    file = open("../../../map/positions.txt", "w")
-    file.write("Next follows the positions registered in the same order than recorded \n")
-    file.close()
 
     # subscribed to joystick inputs on topic "joy"
     rospy.Subscriber("joy", Joy, callback)
